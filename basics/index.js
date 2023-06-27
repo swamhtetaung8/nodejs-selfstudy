91794d075a5a8f93259fb4fdde4afde3d2f4b7f1
Swam Htet Aung <swamhtetaung@Swams-MacBook-Air.local>
2023-06-27 21:54:29 +0630

    day 2:simple api

--- basics/index.js
+++ basics/index.js
@@ -1,4 +1,6 @@
-// const fs = require('fs')
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
 // fs.readFile('txt/final.txt','utf-8',(err,data)=>{
 //   console.log(data)
@@ -10,28 +12,33 @@
 //   console.log('This is time out')
 // }, 3000);
 
-const http = require('http');
-const url = require('url');
-const server = http.createServer((req,res)=>{
+const productData = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const productDataObj = JSON.parse(productData);
+
+const server = http.createServer((req, res) => {
   const path = req.url;
-  console.log(path)
-  switch(path){
-    case "/" :
-      res.end('Overview')
+  console.log(path);
+  switch (path) {
+    case "/":
+      res.end("Overview");
+      break;
+    case "/api":
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(productData);
       break;
     case "/overview":
-      res.end('Overview')
+      res.end("Overview");
       break;
     case "/product":
-      res.end('Product page')
+      res.end("Product page");
       break;
     default:
-      res.writeHead(404)  
-      res.end('Page not found')
+      res.writeHead(404);
+      res.end("Page not found");
       break;
   }
-})
+});
 
-server.listen(8080,'localhost',()=>{
-  console.log('Server listening on port 8080')
+server.listen(8080, "localhost", () => {
+  console.log("Server listening on port 8080");
 });
