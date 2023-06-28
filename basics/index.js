877998bb31c504285848049994eae0bf8cc2aea3
bcmm.intern06 <bcmm.intern06@brycenmyanmar.com.mm>
2023-06-28 10:48:39 +0630

    day 3:making our own modules

--- basics/index.js
+++ basics/index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
-
+const replaceTemp = require('./modules/replaceTemplate')
 // fs.readFile('txt/final.txt','utf-8',(err,data)=>{
 //   console.log(data)
 // })
@@ -12,20 +12,7 @@ const url = require("url");
 //   console.log('This is time out')
 // }, 3000);
 
-const replaceTemp = (temp,product)=> {
-  let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName);
-  output = output.replace(/{%ID%}/g,product.id);
-  output = output.replace(/{%IMAGE%}/g,product.image);
-  output = output.replace(/{%FROM%}/g,product.from);
-  output = output.replace(/{%PRICE%}/g,product.price);
-  output = output.replace(/{%DESCRIPTION%}/g,product.description);
-  output = output.replace(/{%QUANTITY%}/g,product.quantity);
-  output = output.replace(/{%NUTRIENTS%}/g,product.nutrients);
 
-  if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g,'not-organic');
-
-  return output;
-}
 
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8");
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
